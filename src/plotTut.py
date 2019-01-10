33e8013e8cc43be0ed44ef4d53160f3cbf1acc7c
mikinty <myou@andrew.cmu.edu>
2019-01-10 13:03:59 -0500

    not essential parts of project

--- src/plotTut.py
+++
@@ -1,4 +0,0 @@
-import matplotlib.pyplot as plt
-
-plt.plot([1, 2, 3, 4], [1, 4, 9, 25], 'ro')
-plt.show()
--- src/testInteract.py
+++
@@ -1,8 +0,0 @@
-import serial
-
-ard = serial.Serial('/dev/ttyACM4', 9600)
-
-while(True):
-	ard.write(b'1')
-	pause(1)
-	ard.write(b'2')
