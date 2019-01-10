4a93218546f512448b6527e23d62fcc85d35a16b
mikinty <myou@andrew.cmu.edu>
2019-01-10 13:14:05 -0500

    comments

--- src/testMatrix.py
+++ src/testMatrix.py
@@ -1,15 +1,22 @@
+'''
+Simple demonstration of sending matrix information to an arduino
+'''
+
 import serial
 from random import randint
 import time
 import serial.tools.list_ports
 
+# detect and begin serial commuincation with first Arduino
 ports = list(serial.tools.list_ports.comports())
 
 arduinos = [p[0] for p in ports if p[0].startswith('/dev/ttyACM')]
-print(arduinos)
 ard = serial.Serial(arduinos[0], timeout=0)
 
 def generateMatrix():
+    '''
+    Creates a randomly generated 8x8 bit matrix
+    '''
     m = []
     for i in range(8):
         l = []
@@ -20,6 +27,9 @@ def generateMatrix():
     return m
 
 def sendMatrix(m):
+    '''
+    Sends a 8x8 bit matrix to the detected Arduino
+    '''
     ard.write(b's')
     for i in m:
         for j in i:
@@ -27,10 +37,13 @@ def sendMatrix(m):
 
 while(True):
     m = generateMatrix()
+
     print('Printing:')
     print(m)
+
     sendMatrix(m)
     time.sleep(1)
-    print(ard.read(1000))
-    time.sleep(1)
 
+    print(ard.read(1000))
+
+    time.sleep(1)
